3e658f1e4c33d65898a7d24170a3adb2f07136a0
Emma Albury <emma_albury@hotmail.com>
2018-09-10 15:52:02 +0100

    Added top-up method and tests

--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -6,5 +6,16 @@ describe Oystercard do
     it "returns balance of 0" do
       expect(subject.balance).to eq(0)
     end
+
+    describe '#top_do' do
+      it { is_expected.to respond_to(:top_up).with(1).argument }
+
+      it 'tops up the balance' do
+        expect{ subject.top_up 1}.to change{ subject.balance }.by 1
+      end
+    end
   end
 end
+
+# topup = Oystercard.new(20)
+# expect(topup.top_up(amount)).to eq(20)
