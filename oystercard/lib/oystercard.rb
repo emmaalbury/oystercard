8b4c13b764a9c3b45dca0b87b30e5b50cefc2ed3
Emma Albury <emma_albury@hotmail.com>
2018-09-11 22:24:28 +0100

    Exercise progress with Flo and Tom

--- oystercard/lib/oystercard.rb
+++ oystercard/lib/oystercard.rb
@@ -1,15 +1,18 @@
 class Oystercard
 
 MAXIMUM_BALANCE = 90
+MINIMUM_BALANCE = 1
 
-  attr_reader :balance
+  attr_reader :balance, :journey, :entry_station
 
-  def initialize
-    @balance = 0
+  def initialize(balance = 0)
+    @balance = balance
+    @journey = false
+    @entry_station = []
   end
 
   def top_up(amount)
-    fail "The maximum balance of #{MAXIMUM_BALANCE} has been exceeded" if amount + balance > MAXIMUM_BALANCE
+    fail "The maximum balance of #{MAXIMUM_BALANCE} has been exceeded" if amount + @balance > MAXIMUM_BALANCE
     @balance += amount
   end
 
@@ -17,15 +20,65 @@ MAXIMUM_BALANCE = 90
     @balance -= fare
   end
 
-  def touch_in?
-    true
-  end
-
-  def touch_out?
-    true
-  end
-
   def in_journey?
-    true
+    if @entry_station == []
+      return false
+    else
+      return true
+    end
   end
+
+  def touch_in(tube_station)
+    raise "You're not passing with THAT balance" if @balance <= MINIMUM_BALANCE
+    @entry_station << tube_station
+    @journey = true
+  end
+
+  def touch_out(exit)
+    @journey = false
+    @entry_station = nil
+  end
+
 end
+# class Oystercard
+#     attr_reader :balance, :journey, :entry_station
+#
+# Florence, Tom and Em
+#
+#     def initialize(balance = 0)
+#         @balance = balance
+#         @CAPACITY = 90
+#         @MINIMUM = 1
+#         @journey = false
+#         @entry_station = []
+#     end
+#
+#     def top_up(amount)
+#         raise "You cannot top up over £#{@CAPACITY}" if @balance >= @CAPACITY
+#         @balance += amount
+#     end
+#
+#     def deduct(fare)
+#         @balance -= fare
+#     end
+#
+#     def in_journey?
+#       if @entry_station == []
+#         return false
+#       else
+#         return true
+#       end
+#     end
+#
+#     def touch_in(tube_station)
+#         raise "You're not passing with THAT balance" if @balance <= @MINIMUM
+#         @entry_station << tube_station
+#         @journey = true
+#     end
+#
+#     def touch_out(exit)
+#         @journey = false
+#         @entry_station = nil
+#     end
+#
+# end
--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -2,6 +2,84 @@ require 'oystercard'
 
 describe Oystercard do
 
+#Florence, Tom and Em.
+#   require "oystercard"
+#
+# describe Oystercard do
+#     context "creating the oyster card" do
+#
+#         it "expects no error when creating Oystercard instance" do
+#             expect {Oystercard.new}.not_to raise_error
+#         end
+#
+#         it "has a default balance of 0 unless stated otherwise" do
+#             expect(subject.balance).to eq 0
+#         end
+#
+#     end
+#
+#     context "spending and topping up our card" do
+#         it "allows the user to top up money" do
+#             expect(subject.top_up(10)).to eq 10
+#         end
+#
+#         it "after top up, balance is updated" do
+#             subject.top_up(15)
+#             expect(subject.balance).to eq 15
+#         end
+#
+#         let(:card) { described_class.new(90) }
+#
+#         it "raises error when balance is more than 90" do
+#             expect {card.top_up(1)}.to raise_error("You cannot top up over £90")
+#         end
+#
+#         it "deducts money from my card balance" do
+#             expect(card.deduct(23)).to eq 67
+#         end
+#     end
+#
+#     context "touching in and out, and checking if it's in journey" do
+#         let(:card) { described_class.new }
+#         let(:tube_station) { double :tube_station}
+#         let(:tube_exit) { double :tube_exit}
+#
+#
+#
+#         it "determines if card is on a journey" do
+#             #sallow(card).to receive() { false }
+#             expect(card.in_journey?).to eq false
+#         end
+#
+#         it "touches in, turning journey to true" do
+#             card = described_class.new(10)
+#             allow(card).to receive(:in_journey?) { false }
+#             expect(card.touch_in(tube_station)).to eq true    #to be_truthy
+#         end
+#
+#         it "touches out, turning journey to false" do
+#             allow(card).to receive(:in_journey?) { true }
+#             expect(card.touch_out(tube_exit)).to be_falsey
+#         end
+#
+#         it "raises error if touching in with balance of less than 1" do
+#           oys = Oystercard.new
+#             expect { oys.touch_in("station") }.to raise_error("You're not passing with THAT balance")
+#         end
+#
+#         it "remembers entry station after touch in" do
+#           card.top_up(5)
+#             card.touch_in(tube_station)
+#            expect(card.entry_station).to eq [tube_station]
+#         end
+#
+#
+#     end
+  context "creating the oyster card" do
+    it "expects no error when creating Oystercard instance" do
+      expect {Oystercard.new}.not_to raise_error
+   end
+
   context "balance on oystercard" do
     it "returns balance of 0" do
       expect(subject.balance).to eq(0)
@@ -29,31 +107,36 @@ describe Oystercard do
     end
   end
 
+    context "declares state of use" do
+      let(:card) { described_class.new }
+       let(:tube_station) { double :tube_station}
+       let(:tube_exit) { double :tube_exit}
 
-  context "declares state of use" do
+     it "determines if card is on a journey" do
+        expect(card.in_journey?).to eq false
+      end
 
-    describe '#touch_in?' do
-      it "returns true if card is in use" do
-      expect(subject.touch_in?).to be true
+      it "touches in, turning journey to true" do
+        card = described_class.new(10)
+        allow(card).to receive(:in_journey?) { false }
+        expect(card.touch_in(tube_station)).to be true
+      end
+
+      it "touches out, turning journey to false" do
+          allow(card).to receive(:in_journey?) { true }
+          expect(card.touch_out(tube_exit)).to be_falsey
+      end
+
+      it "raises error if touching in with balance of less than 1" do
+          oys = Oystercard.new
+          expect { oys.touch_in("station") }.to raise_error("You're not passing with THAT balance")
+        end
+
+        it "remembers entry station after touch in" do
+          card.top_up(5)
+          card.touch_in(tube_station)
+          expect(card.entry_station).to eq [tube_station]
+        end
       end
     end
-
-    describe '#touch_out?' do
-      it "returns true if card is not in use" do
-      expect(subject.touch_out?).to be true
-      end
-    end
-
-    describe '#in_journey?' do
-      it "returns true if card is not in use" do
-      expect(subject.in_journey?).to be true
-      end
-    end
-  end
 end
-
-# topup = Oystercard.new(20)
-# expect(topup.top_up(amount)).to eq(20)
-
-
-#      allow(subject).to receive(:touch_in?).and_return true
