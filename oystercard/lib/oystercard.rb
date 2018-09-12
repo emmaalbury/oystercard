89d566484961647b9cc7ba0e751b9fceff586eb9
Emma Albury <emma_albury@hotmail.com>
2018-09-12 17:27:41 +0100

    debugged and created hash for journey

--- oystercard/lib/oystercard.rb
+++ oystercard/lib/oystercard.rb
@@ -2,13 +2,15 @@ class Oystercard
 
 MAXIMUM_BALANCE = 90
 MINIMUM_BALANCE = 1
+STARTING_BALANCE = 0
 
-  attr_reader :balance, :journey, :entry_station
+  attr_reader :balance, :journey, :beginning_station, :store_journey
 
-  def initialize(balance = 0)
-    @balance = balance
-    @journey = false
-    @entry_station = []
+  def initialize(starting_balance = STARTING_BALANCE)
+    @balance = starting_balance
+    @beginning_station = nil
+    @store_journey = []
+
   end
 
   def top_up(amount)
@@ -20,65 +22,16 @@ MINIMUM_BALANCE = 1
     @balance -= fare
   end
 
-  def in_journey?
-    if @entry_station == []
-      return false
-    else
-      return true
-    end
-  end
-
   def touch_in(tube_station)
     raise "You're not passing with THAT balance" if @balance <= MINIMUM_BALANCE
-    @entry_station << tube_station
-    @journey = true
+    @beginning_station = tube_station
   end
 
-  def touch_out(exit)
-    @journey = false
-    @entry_station = nil
+  def touch_out(exit_station)
+    store_journey << { entry_station: beginning_station, exit_station: exit_station }
   end
 
+  def in_journey?
+    @beginning_station != nil
+  end
 end
-# class Oystercard
-#     attr_reader :balance, :journey, :entry_station
-#
-# Florence, Tom and Em
-#
-#     def initialize(balance = 0)
-#         @balance = balance
-#         @CAPACITY = 90
-#         @MINIMUM = 1
-#         @journey = false
-#         @entry_station = []
-#     end
-#
-#     def top_up(amount)
-#         raise "You cannot top up over £#{@CAPACITY}" if @balance >= @CAPACITY
-#         @balance += amount
-#     end
-#
-#     def deduct(fare)
-#         @balance -= fare
-#     end
-#
-#     def in_journey?
-#       if @entry_station == []
-#         return false
-#       else
-#         return true
-#       end
-#     end
-#
-#     def touch_in(tube_station)
-#         raise "You're not passing with THAT balance" if @balance <= @MINIMUM
-#         @entry_station << tube_station
-#         @journey = true
-#     end
-#
-#     def touch_out(exit)
-#         @journey = false
-#         @entry_station = nil
-#     end
-#
-# end
--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -2,83 +2,11 @@ require 'oystercard'
 
 describe Oystercard do
 
-#Florence, Tom and Em.
-#   require "oystercard"
-#
-# describe Oystercard do
-#     context "creating the oyster card" do
-#
-#         it "expects no error when creating Oystercard instance" do
-#             expect {Oystercard.new}.not_to raise_error
-#         end
-#
-#         it "has a default balance of 0 unless stated otherwise" do
-#             expect(subject.balance).to eq 0
-#         end
-#
-#     end
-#
-#     context "spending and topping up our card" do
-#         it "allows the user to top up money" do
-#             expect(subject.top_up(10)).to eq 10
-#         end
-#
-#         it "after top up, balance is updated" do
-#             subject.top_up(15)
-#             expect(subject.balance).to eq 15
-#         end
-#
-#         let(:card) { described_class.new(90) }
-#
-#         it "raises error when balance is more than 90" do
-#             expect {card.top_up(1)}.to raise_error("You cannot top up over £90")
-#         end
-#
-#         it "deducts money from my card balance" do
-#             expect(card.deduct(23)).to eq 67
-#         end
-#     end
-#
-#     context "touching in and out, and checking if it's in journey" do
-#         let(:card) { described_class.new }
-#         let(:tube_station) { double :tube_station}
-#         let(:tube_exit) { double :tube_exit}
-#
-#
-#
-#         it "determines if card is on a journey" do
-#             #sallow(card).to receive() { false }
-#             expect(card.in_journey?).to eq false
-#         end
-#
-#         it "touches in, turning journey to true" do
-#             card = described_class.new(10)
-#             allow(card).to receive(:in_journey?) { false }
-#             expect(card.touch_in(tube_station)).to eq true    #to be_truthy
-#         end
-#
-#         it "touches out, turning journey to false" do
-#             allow(card).to receive(:in_journey?) { true }
-#             expect(card.touch_out(tube_exit)).to be_falsey
-#         end
-#
-#         it "raises error if touching in with balance of less than 1" do
-#           oys = Oystercard.new
-#             expect { oys.touch_in("station") }.to raise_error("You're not passing with THAT balance")
-#         end
-#
-#         it "remembers entry station after touch in" do
-#           card.top_up(5)
-#             card.touch_in(tube_station)
-#            expect(card.entry_station).to eq [tube_station]
-#         end
-#
-#
-#     end
   context "creating the oyster card" do
     it "expects no error when creating Oystercard instance" do
       expect {Oystercard.new}.not_to raise_error
-   end
+    end
+  end
 
   context "balance on oystercard" do
     it "returns balance of 0" do
@@ -98,45 +26,61 @@ describe Oystercard do
         expect{ subject.top_up 1 }.to raise_error("The maximum balance of #{max} has been exceeded")
       end
     end
-
-    describe '#deduct' do
-      it 'deducts fare amount from balance' do
-        subject.top_up(20)
-        expect{ subject.deduct 10 }.to change{ subject.balance }.by -10
-      end
-    end
   end
+
+    # describe '#deduct' do
+    #   it 'deducts fare amount from balance' do
+  #   #     subject.top_up(20)
+  #   #     expect{ subject.deduct 10 }.to change{ subject.balance }.by -10
+  #   #   end
+  #   # end
+  # end
 
     context "declares state of use" do
       let(:card) { described_class.new }
-       let(:tube_station) { double :tube_station}
-       let(:tube_exit) { double :tube_exit}
+       # let(:tube_station) { double :tube_station}
+       # let(:tube_exit) { double :tube_exit}
 
-     it "determines if card is on a journey" do
-        expect(card.in_journey?).to eq false
-      end
+     # it "determines if card is on a journey" do
+     #    expect(card.in_journey?).to eq false
+     #  end
 
-      it "touches in, turning journey to true" do
-        card = described_class.new(10)
-        allow(card).to receive(:in_journey?) { false }
-        expect(card.touch_in(tube_station)).to be true
-      end
-
-      it "touches out, turning journey to false" do
-          allow(card).to receive(:in_journey?) { true }
-          expect(card.touch_out(tube_exit)).to be_falsey
-      end
+      # it "touches in, turning journey to true" do
+      #   card = described_class.new(10)
+      #   allow(card).to receive(:in_journey?) { false }
+      #   expect(card.touch_in(tube_station)).to be true
+      # end
+      #
+      #  it "touches out, turning journey to false" do
+      #      allow(card).to receive(:in_journey?) { nil }
+      #      expect(card.in_journey?).to be false
+      #  end
 
       it "raises error if touching in with balance of less than 1" do
           oys = Oystercard.new
           expect { oys.touch_in("station") }.to raise_error("You're not passing with THAT balance")
-        end
+      end
 
-        it "remembers entry station after touch in" do
-          card.top_up(5)
-          card.touch_in(tube_station)
-          expect(card.entry_station).to eq [tube_station]
-        end
+      it "remembers entry station after touch in" do
+        card.top_up(5)
+        card.touch_in("Manor House")
+        expect(card.beginning_station).to eq "Manor House"
+      end
+
+      # it "touch_out accepts exit station as argument" do
+      #   card.touch_out("angel")
+      #   expect(card.touch_out).to eq "angel"
+      # end
+
+      it "begins with an empty list for journeys" do
+        expect(card.store_journey).to eq []
+      end
+
+      it "stores journey in a hash in touch_out" do
+        card.top_up(5)
+        card.touch_in("Hammersmith")
+        card.touch_out("Liverpool")
+        expect(card.store_journey).to eq([{ entry_station: "Hammersmith", exit_station: "Liverpool" }])
       end
     end
 end
